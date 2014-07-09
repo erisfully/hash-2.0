070e035dd9a5a15114ddb61ef360d6bc68bad434
Ashley <a.clarkem@gmail.com>
2014-07-09 10:09:13 -0600

    end of warm up time missing last test

---
+++ lib/dog_hash.rb
@@ -0,0 +1,34 @@
+
+
+DOG = {
+      cattle_dog:{
+        preferences:
+                [{name: "Harleigh"}],
+        names: ['Harleigh','brown']
+  },
+    "harleigh" => {
+      preferences: [{
+        favorite_toy: "frog" }]
+  },
+    "trixie" => {
+      preferences: [{
+                      favorite_food: "steak" }]
+    }
+}
+
+
+
+
+
+
+
+
+# PEOPLE = {
+#   "joe" => {
+#     first_name: "Joe",
+#     last_name: "Jenkins",
+#     preferences: {
+#       meal: "meat",
+#       favorite_genres: ["jazz", "soul"],
+#     }
+#
